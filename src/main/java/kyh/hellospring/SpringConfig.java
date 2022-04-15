f75cafa73bf444ef89ab7a6901fff4be824adc5f
Hwang Nae Gwon <joonwoo9192@gmail.com>
2022-04-15 16:32:36 +0900

    추가

--- src/main/java/kyh/hellospring/SpringConfig.java
+++ src/main/java/kyh/hellospring/SpringConfig.java
@@ -1,13 +1,22 @@
 package kyh.hellospring;
 
-import kyh.hellospring.repository.MemberRepository;
-import kyh.hellospring.repository.MemoryMemberRepository;
+import kyh.hellospring.repository.*;
 import kyh.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -16,7 +25,10 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+
+//        return new MemoryMemberRepository();
+//        return new JdbcMemberRepository(dataSource);
+        return new JdbcTempleteMemberRepository(dataSource);
     }
 
 }
---
+++ src/test/java/kyh/hellospring/service/MemberServiceIntegrationTest.java
@@ -0,0 +1,55 @@
+package kyh.hellospring.service;
+
+import kyh.hellospring.domain.Member;
+import kyh.hellospring.repository.MemberRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+@Transactional
+class MemberServiceIntegrationTest {
+
+    @Autowired
+    MemberService    memberService;
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    void 회원가입() {
+        //given
+        Member member = new Member();
+        member.setName("dummyMember");
+
+        //when
+        Long saveId = memberService.join(member);
+
+        //then
+        Member findMember = memberRepository.findById(saveId).get();
+        assertThat(member.getName()).isEqualTo(findMember.getName());
+
+    }
+
+    @Test
+    public void 중복회원예외() {
+        //given
+        Member member1 = new Member();
+        member1.setName("spring1");
+        Member member2 = new Member();
+        member2.setName("spring1");
+
+        //when
+        memberService.join(member1);
+        IllegalStateException e = assertThrows(IllegalStateException.class, () -> {
+            memberService.join(member2);
+        });
+
+        assertThat(e.getMessage()).isEqualTo("Already Exists Member");
+
+    }
+
+}
